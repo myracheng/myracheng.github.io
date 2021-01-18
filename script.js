8bcfb7c010cc25028c541d29d41d435476f32aeb
Myra Cheng <myracheng027@gmail.com>
2021-01-18 15:22:40 -0800

    added home

---
+++ script.js
@@ -0,0 +1,20 @@
+// Typewriter JS Code
+var app = document.getElementById('intro');
+
+var typewriter = new Typewriter(app, {
+    delay: 75
+});
+
+typewriter.typeString('Hi, I’m Myra. ')
+    .pauseFor(1000)
+    .typeString('I like to read a lot and write a little.')
+    .callFunction(function() {
+        $(".Typewriter__cursor").hide();
+    })
+    .start();
+
+// Google Color Cycler Code
+var colors = ['#4285F4', '#EA4335', '#FBBC04', '#34A853']
+$(".google").hover(function() {
+    this.style.setProperty("--hov", colors[Math.floor(Math.random()*colors.length)]);
+})
