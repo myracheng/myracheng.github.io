f4069473d5a13a71b1fb8852bae549f5354ed961
Myra Cheng <myracheng027@gmail.com>
2021-01-18 16:11:02 -0800

    k gotta take break for now

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 window.onload = choosePic;
 
-var myPix = new Array("gifs/bird.gif","gifs/bk.gif","gifs/ph.gif","gifs/snail.gif","gifs/tonton.gif","gifs/ttq.gif");
+var myPix = new Array("gifs/bird.gif","gifs/bk.gif","gifs/ph.gif","gifs/snail.gif","gifs/ttq.gif");
 
 function choosePic() {
      var randomNum = Math.floor(Math.random() * myPix.length);
      document.getElementById("logo").src = myPix[randomNum];
-
+}
 // Typewriter JS Code
 var app = document.getElementById('intro');
 
@@ -20,9 +20,3 @@ typewriter.typeString('Hi, I’m Myra. ')
         $(".Typewriter__cursor").hide();
     })
     .start();
-
-// Google Color Cycler Code
-var colors = ['#4285F4', '#EA4335', '#FBBC04', '#34A853']
-$(".google").hover(function() {
-    this.style.setProperty("--hov", colors[Math.floor(Math.random()*colors.length)]);
-})
